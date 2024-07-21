0a6e20159bdbdd642af9e5b0dbafb815044c44c3
Az1z67 <abbasovaziz61@gmail.com>
2024-07-21 16:20:05 +0400

    commit

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const result = [];
+  let index = 0;
+  for (const value of array) {
+    result[index] = appendString + value;
+    index += 1;
+  }
+  return result;
+}
