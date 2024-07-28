6395a0e29abeec202a012549d6a99a5aa4377182
Az1z67 <abbasovaziz61@gmail.com>
2024-07-28 17:00:48 +0400

    commit

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,6 @@ export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string') {
 	return '';
   }
-
   return [...set]
 	.filter(value => value.startsWith(startString))
 	.map(value => value.slice(startString.length))
