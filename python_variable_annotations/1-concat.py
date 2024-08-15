e8d248d8bd60bf5e00dc894dab5fa47e0faf17cd
Az1z67 <abbasovaziz61@gmail.com>
2024-08-15 13:38:31 +0400

    commit

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Annotating with str
+"""
+
+def contact(tr1: str, str2: str) -> str:
+    """
+    Function concatenates two strings
+    """
+    return "{}{}".format(str1, str2)
