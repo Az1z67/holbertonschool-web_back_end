76bcd8e541e8c3fd1f4e78b96809749d853ebf39
Az1z67 <abbasovaziz61@gmail.com>
2024-08-15 18:25:12 +0400

    commiy

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,21 +1,4 @@
 #!/usr/bin/env python3
-"""
-Annotating
-"""
-from typing import Union, Any, TypeVar
-
-
-def safely_get_value(dct: dict, key: Any, default: Union[TypeVar('T'), None] = None) -> Union[Any, TypeVar('T')]:
-    """
-    Function takes a dictionary, key and default value and returns the value
-    """
-    if key in dct:
-        return dct[key]
-    else:
-        return default
-
-
-#!/usr/bin/env python3
 '''
 Module Doc
 '''
