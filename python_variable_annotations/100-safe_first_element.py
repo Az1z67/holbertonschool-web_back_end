b11e687648c40f4f441d7e35c9f9b37ec94ae3cf
Az1z67 <abbasovaziz61@gmail.com>
2024-08-15 18:16:40 +0400

    commit

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -5,6 +5,7 @@ Annotating variables and function with complex types
 
 from typing import Sequence, Union, Any, TypeVar, List
 
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
     Function takes a list of any type and returns its first element
