7dfcdb2e5a6f2825bc2992c2110092bc5b81e3fc
Az1z67 <abbasovaziz61@gmail.com>
2024-08-15 15:22:43 +0400

    commit

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Annotating with float adn int
+""" 
+def floor(n: float) -> int:
+    """
+    Function takes float n and returns the floor of n
+    """
+    return int(n)
