34230555650ddd912336dbd720e75a5e05e3577a
Az1z67 <abbasovaziz61@gmail.com>
2024-08-15 15:26:54 +0400

    commit

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Annotating with float and str
+"""
+
+def to_str(n: float) -> str:
+    """
+    Function takes float n and returns the string representation of n
+    """
+    return str(n)
