fc81b937dd37ec8cc3a9f1949ec064b5d9859a75
Az1z67 <abbasovaziz61@gmail.com>
2024-08-15 15:39:42 +0400

    commit

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Annotating
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Function takes list of floats and returns their sum
+    """
+    return sum(mxd_lst)
