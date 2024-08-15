3e19758d1355c275f541d4aa6cb2f0b5f49201d2
Az1z67 <abbasovaziz61@gmail.com>
2024-08-15 15:33:40 +0400

    commit

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Annotating
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Function takes list of floats and returns their sum
+    """
+    return sum(input_list)
