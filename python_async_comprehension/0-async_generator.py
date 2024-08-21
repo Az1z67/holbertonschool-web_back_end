64cadab6af0c5763356086ed1df444bb607e639e
Az1z67 <abbasovaziz61@gmail.com>
2024-08-21 18:29:26 +0400

    commit

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,9 +5,9 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """Async generator that loops 10 times"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-        
