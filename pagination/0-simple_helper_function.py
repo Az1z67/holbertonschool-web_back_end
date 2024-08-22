db915cfcf18e938f262488438e4885e324b26774
Az1z67 <abbasovaziz61@gmail.com>
2024-08-22 16:08:42 +0400

    commit

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""perform pagination"""
+
+def index_range(page: int, page_size: int) -> tuple:
+    """perform pagination"""
+    return ((page - 1) * page_size, page * page_size)
