bc25c96d72bebe073cccc276274393ef99971a96
Az1z67 <abbasovaziz61@gmail.com>
2024-08-25 17:38:02 +0400

    commit

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
-"""
-Insert
-"""
+""" Insert document """
 import pymongo
 
 
 def insert_school(mongo_collection, **kwargs):
+    """ Insert document
     """
-    Insert
-    """
-    if len(kwargs) == 0:
-        return None
-    return mongo_collection.insert(kwargs)
+    new_school = mongo_collection.insert_one(kwargs)
+
+    return (new_school.inserted_id)
